d27b8bbc178082bb6edb61f9c28429193b46dfde
Arpan Konar <arpan97official@gmail.com>
2020-04-07 16:21:08 +0530

    Initialized assignment 2

---
+++ PacketSwitch/src/PacketSwitchController.java
@@ -0,0 +1,9 @@
+import java.io.FileNotFoundException;
+
+//Main entry point of the program
+public class PacketSwitchController {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
